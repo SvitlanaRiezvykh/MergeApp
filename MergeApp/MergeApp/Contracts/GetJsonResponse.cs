ecb32d062d26b404b17a94dc58f3c2b6833aca15
Svitlana Riezvykh <s.riezvykh@draftkings.com>
2021-11-14 15:47:21 +0200

    Define contracts

---
+++ MergeApp/MergeApp/Contracts/GetJsonResponse.cs
@@ -0,0 +1,11 @@
+﻿using Newtonsoft.Json;
+using System.Collections.Generic;
+
+namespace MergeApp.Contracts
+{
+    public partial class GetJsonResponse
+    {
+        [JsonProperty("ranked")]
+        public List<Ranked> Ranked { get; set; }
+    }
+}
---
+++ MergeApp/MergeApp/Contracts/Ranked.cs
@@ -0,0 +1,14 @@
+﻿using Newtonsoft.Json;
+using System.Collections.Generic;
+
+namespace MergeApp.Contracts
+{
+    public partial class Ranked
+    {
+        [JsonProperty("priority")]
+        public long Priority { get; set; }
+
+        [JsonProperty("vals")]
+        public IDictionary<string, object> Vals { get; set; }
+    }
+}
